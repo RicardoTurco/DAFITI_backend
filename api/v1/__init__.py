c7259383f4689b57437988d4f881f3e17a42bcd5
RicardoTurco <ricturco@gmail.com>
2022-05-03 10:04:30 -0300

    add: Resource Pessoas

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -23,7 +23,9 @@ api = Api(v1_blueprint,
 
 from .resources.tipo_contas.tipo_conta import api as tipo_contas_ns
 from .resources.tipo_transacoes.tipo_transacao import api as tipo_transacoes_ns
+from .resources.pessoas.pessoa import api as pessoas_ns
 
 
 api.add_namespace(tipo_contas_ns)
 api.add_namespace(tipo_transacoes_ns)
+api.add_namespace(pessoas_ns)
